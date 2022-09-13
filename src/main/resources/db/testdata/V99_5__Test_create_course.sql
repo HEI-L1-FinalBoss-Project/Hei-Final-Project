1c60f1179876cdb54ed39ecd8decb23787d094eb
mayah <hei.mayah.3@gmail.com>
2022-09-13 10:12:02 +0300

    feat: add in CourseIT

---
+++ src/main/resources/db/testdata/V99_5__Test_create_course.sql
@@ -0,0 +1,4 @@
+insert into "group"
+    (id,ref, name,credits,total_hours)
+values ('course1_id', 'CRS21001', 'course1_name', 10000,10),
+       ('course2_id', 'CRS21002', 'course2_name', 20000,12);
